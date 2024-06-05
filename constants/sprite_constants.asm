6e98748bcc17d4ed32741914d04439f735c44580
Alex Fisher <fisheralex92@hotmail.com>
2024-06-05 01:13:57 +0100

    New data type for rattata to get colour correct

--- constants/sprite_constants.asm
+++ constants/sprite_constants.asm
@@ -178,6 +178,7 @@ SPRITE_POKEMON EQU const_value
 	const SPRITE_SENTRET
 	const SPRITE_MEW
 	const SPRITE_KANGASKHAN
+	const SPRITE_RATTATA_UP
 NUM_POKEMON_SPRITES EQU const_value - SPRITE_POKEMON
 
 ; special GetMonSprite values (see engine/overworld/overworld.asm)
--- data/sprites/sprite_mons.asm
+++ data/sprites/sprite_mons.asm
@@ -63,4 +63,5 @@ SpriteMons:
 	db SENTRET
 	db MEW
 	db KANGASKHAN
+	db RATTATA_UP
 	assert_table_length NUM_POKEMON_SPRITES
