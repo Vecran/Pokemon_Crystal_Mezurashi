8a4d577dea64dcf69238adfbd032d6431069a3e9
Alex Fisher <fisheralex92@hotmail.com>
2024-06-22 22:39:27 +0100

    FadeOutToBlack doesn't exist, should use FadeBlackQuickly

--- maps/KurtsHouse.asm
+++ maps/KurtsHouse.asm
@@ -168,7 +168,7 @@ Kurt1:
 	writetext KurtsHouseKurtGetStartedText
 	waitbutton
 	closetext
-	special FadeOutToBlack
+	special FadeBlackQuickly
 	special ReloadSpritesNoPalettes
 	playsound SFX_WARP_TO
 	waitsfx
